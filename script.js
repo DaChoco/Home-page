d927c7c42ce2d6d391441b0baf4fe299ba36456d
DaChoko <Mailechokoe2@gmail.com>
2024-11-20 00:33:57 +0200

    Made the project more responsive for modern phones

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = function(){ //loading animation?
     document.body.classList.add('loaded');
 }
+
+function toggleMenu(){
+    const menuLinks = document.querySelector('.nav-links');
+    menuLinks.classList.toggle('show');
+}
