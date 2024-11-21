6503ca5ae3c56f3ae217a09f68e35c45c28e6616
DaChoko <Mailechokoe2@gmail.com>
2024-11-21 23:14:07 +0200

    implemented session storage for side bars

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-window.onload = function(){ //loading animation?
-    document.body.classList.add('loaded');
-}
-
-function toggleMenu(){
+function openMenu(){
     const sidebar = document.querySelector('.sidenav');
     sidebar.style.display = 'flex';
+
+    sessionStorage.setItem('clicked', true);
     
 }
+
+function closeMenu(){
+    const sidebar = document.querySelector('.sidenav');
+    sidebar.style.display = 'none';
+
+    sessionStorage.removeItem('clicked')
+    
+}
+
+window.onload = function(){ //loading animation?
+    var is_visible;
+    document.body.classList.add('loaded');
+
+    is_visible = sessionStorage.getItem('clicked');
+
+    if (is_visible == 'true'){
+        openMenu();
+    }
+
+    
+}
+
