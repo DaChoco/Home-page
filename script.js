7920c7f397394b62c77c170223ae638899c528dd
DaChoko <Mailechokoe2@gmail.com>
2024-11-22 14:11:16 +0200

    For now, finished with responsiveness testing

--- script.js
+++ script.js
@@ -14,25 +14,31 @@ function closeMenu(){
     
 }
 
-function formExtract(){
+function formExtract(){ //To extract text from this document
     var name, email, strText;
+    const errorMSG = document.getElementById('error');
+
+    //extractions
 
     name = document.getElementById('name').value;
     email = document.getElementById('emailAd').value;
     strText = document.getElementById('Write').value;
 
+
+
+    //Testing if the Email is proper
     if (!email.includes('@')){
-        window.alert("Sorry, but that is not a proper email address. Ensure it contains the @ symbol and a domain such as gmail.");
+        errorMSG.style.display = 'flex';
+
+        setTimeout( () => {
+            errorMSG.style.display = 'none';
+        }, 5000)
         document.getElementById('emailAd').value = "";
     }
     else{
+
         console.log(name + "\n" + email + "\n" + strText);
-    }
-
-
-    
-    
-
+    }  
 }
 
 window.onload = function(){ //loading animation?
