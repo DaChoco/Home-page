6f2f3f4118370d027426b93470fe23fdf08d4af3
DaChoko <Mailechokoe2@gmail.com>
2024-11-22 12:57:22 +0200

    Made basic string extraction in Email

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ function closeMenu(){
     
 }
 
+function formExtract(){
+    var name, email, strText;
+
+    name = document.getElementById('name').value;
+    email = document.getElementById('emailAd').value;
+    strText = document.getElementById('Write').value;
+
+    if (!email.includes('@')){
+        window.alert("Sorry, but that is not a proper email address. Ensure it contains the @ symbol and a domain such as gmail.");
+        document.getElementById('emailAd').value = "";
+    }
+    else{
+        console.log(name + "\n" + email + "\n" + strText);
+    }
+
+
+    
+    
+
+}
+
 window.onload = function(){ //loading animation?
     var is_visible;
     document.body.classList.add('loaded');
